7ef6a7d08269bb4a2d522cd70c7b07ca331aab8f
luminupled <somarur8@gmail.com>
2020-01-03 12:33:57 +0530

    changes

--- src/app/businessplanprojectreport/businessplanprojectreport.component.ts
+++ src/app/businessplanprojectreport/businessplanprojectreport.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpErrorResponse }    from '@angular/common/http';
+import { EventEmitter } from 'events';
+import { FormService } from '../_service/form.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-businessplanprojectreport',
@@ -8,10 +12,44 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class BusinessplanprojectreportComponent implements OnInit {
 
-  constructor() { }
+  title:"Ram";
+  posts: any;
+  data: any;
+
+  constructor(private products: FormService, private router: Router) { }
 
   ngOnInit() {
+    this.getdata();
   }
 
+  login = new FormGroup({
+    name : new FormControl('',Validators.required),
+    email : new FormControl('', [
+      Validators.required,
+      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+    ]),
+    mobile : new FormControl('', Validators.required),
+ }
+ );
+
+
+ getdata(){
+  this.products.getdata().subscribe(
+    res => {
+      this.posts = res;
+      console.log(this.posts);
+    }
+  );
+}
+
+onSubmit(){
+  this.products.adddata(this.login.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.login.reset();
+}
 
 }
