f138bcf43bfed50458de63c316e289d7fe5c6bee
DharamCA <sonamrur8@gmail.com>
2020-01-07 13:00:59 +0530

    changes

--- src/app/fssairegistration/fssairegistration.component.ts
+++ src/app/fssairegistration/fssairegistration.component.ts
@@ -15,6 +15,12 @@ export class FssairegistrationComponent implements OnInit {
   title:"Ram";
   posts: any;
   data: any;
+  subject1 = "Arrange a call about 'business plan project report'";
+  message1 = "I am Still confused about 'business plan project report', please arrange a call for the same.";
+  subject2 = "Still confused about 'business plan project report'";
+  message2 = "Still confused about 'business plan project report', please provide Free Advisor Consultation.";
+  subject3 = "Complete List of Document required";
+  message3 = "Please share the list of required document for the same.";
 
   constructor(private products: FormService, private router: Router) { }
 
@@ -29,8 +35,34 @@ export class FssairegistrationComponent implements OnInit {
       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
     ]),
     mobile : new FormControl('', Validators.required),
+    subject : new FormControl(''),
+    message : new FormControl(''),
  }
  );
+
+ logins = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+loginss = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
 
 
  getdata(){
@@ -44,6 +76,26 @@ export class FssairegistrationComponent implements OnInit {
 
 onSubmit(){
   this.products.adddata(this.login.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.login.reset();
+}
+
+onSubmit1(){
+  this.products.adddata(this.logins.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.login.reset();
+}
+
+onSubmit2(){
+  this.products.adddata(this.loginss.value)
   .subscribe(data => {
     console.log(this.data);
     
