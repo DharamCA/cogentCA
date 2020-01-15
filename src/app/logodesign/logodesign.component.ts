b545fe4937d7fa767d961add733769e27b751f58
Digitalpublish <somarur8@gmail.com>
2020-01-15 13:57:56 +0530

    changes

--- src/app/logodesign/logodesign.component.ts
+++ src/app/logodesign/logodesign.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpErrorResponse }    from '@angular/common/http';
+import { EventEmitter } from 'events';
+import { FormService } from '../_service/form.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logodesign',
@@ -7,9 +12,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogodesignComponent implements OnInit {
 
-  constructor() { }
+  title:"Ram";
+  posts: any;
+  data: any;
+  subject1 = "Arrange a call about 'business plan project report'";
+  message1 = "I am Still confused about 'business plan project report', please arrange a call for the same.";
+  subject2 = "Still confused about 'business plan project report'";
+  message2 = "Still confused about 'business plan project report', please provide Free Advisor Consultation.";
+  subject3 = "Complete List of Document required";
+  message3 = "Please share the list of required document for the same.";
+  subject4 = "Not Founs Ansrwe our Question";
+  message4 = "Not Founs Ansrwe our Question. Please arrange a call for the same.";
+
+  constructor(private products: FormService, private router: Router) { }
 
   ngOnInit() {
+    this.getdata();
   }
+
+  login = new FormGroup({
+    name : new FormControl('',Validators.required),
+    email : new FormControl('', [
+      Validators.required,
+      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+    ]),
+    mobile : new FormControl('', Validators.required),
+    subject : new FormControl(''),
+    message : new FormControl(''),
+ }
+ );
+
+ logins = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+loginss = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+loginsss = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+
+ getdata(){
+  this.products.getdata().subscribe(
+    res => {
+      this.posts = res;
+      console.log(this.posts);
+    }
+  );
+}
+
+onSubmit(){
+  this.products.adddata(this.login.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.login.reset();
+}
+
+onSubmit1(){
+  this.products.adddata(this.logins.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.logins.reset();
+}
+
+onSubmit2(){
+  this.products.adddata(this.loginss.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.loginss.reset();
+}
+
+onSubmit3(){
+  this.products.adddata(this.loginsss.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.loginsss.reset();
+}
 
 }
--- src/app/trademark/trademark.component.ts
+++ src/app/trademark/trademark.component.ts
@@ -11,11 +11,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./trademark.component.css']
 })
 export class TrademarkComponent implements OnInit {
-
+ 
   
   title:"Ram";
   posts: any;
   data: any;
+  subject1 = "Arrange a call about 'business plan project report'";
+  message1 = "I am Still confused about 'business plan project report', please arrange a call for the same.";
+  subject2 = "Still confused about 'business plan project report'";
+  message2 = "Still confused about 'business plan project report', please provide Free Advisor Consultation.";
+  subject3 = "Complete List of Document required";
+  message3 = "Please share the list of required document for the same.";
+  subject4 = "Not Founs Ansrwe our Question";
+  message4 = "Not Founs Ansrwe our Question. Please arrange a call for the same.";
 
   constructor(private products: FormService, private router: Router) { }
 
@@ -30,8 +38,46 @@ export class TrademarkComponent implements OnInit {
       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
     ]),
     mobile : new FormControl('', Validators.required),
+    subject : new FormControl(''),
+    message : new FormControl(''),
  }
  );
+
+ logins = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+loginss = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
+
+loginsss = new FormGroup({
+  name : new FormControl('',Validators.required),
+  email : new FormControl('', [
+    Validators.required,
+    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$")
+  ]),
+  mobile : new FormControl('', Validators.required),
+  subject : new FormControl(''),
+  message : new FormControl(''),
+}
+);
 
 
  getdata(){
@@ -51,6 +97,36 @@ onSubmit(){
   })
   alert("Data submitted successfuly");
     this.login.reset();
+}
+
+onSubmit1(){
+  this.products.adddata(this.logins.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.logins.reset();
+}
+
+onSubmit2(){
+  this.products.adddata(this.loginss.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.loginss.reset();
+}
+
+onSubmit3(){
+  this.products.adddata(this.loginsss.value)
+  .subscribe(data => {
+    console.log(this.data);
+    
+  })
+  alert("Data submitted successfuly");
+    this.loginsss.reset();
 }
 
 }
